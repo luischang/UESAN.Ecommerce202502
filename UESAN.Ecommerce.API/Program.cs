30f8565139a2fe8a319ee97c6770939fc2525f50
Luis Chang <luis.chang@outlook.com>
2025-10-31 21:19:40 -0500

    Añadir política de CORS "AllowAll"
    Se ha implementado una política de CORS llamada "AllowAll" en el archivo `Program.cs`, que permite cualquier origen, método y encabezado. Además, se ha configurado la aplicación para utilizar esta política mediante `app.UseCors("AllowAll")`, asegurando que se aplique a todas las solicitudes entrantes.

--- UESAN.Ecommerce.API/Program.cs
+++ UESAN.Ecommerce.API/Program.cs
@@ -26,6 +26,20 @@ builder.Services.AddDbContext<StoreDbContext>(
     options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
+
+//Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder//.WithOrigins("URL-frontend")
+                .AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -38,6 +52,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
